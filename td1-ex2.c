fa581286296acce35fc700befd58b9f7b1c69dfa
Timothee Craig <timothee.craig@gmail.com>
2016-10-14 15:47:39 +0200

    Fix a bug
    I don't even know why I did this mistake in the first place...

--- td1-ex2.c
+++ td1-ex2.c
@@ -16,7 +16,7 @@ int main(){
     do { 
       printf("Enter un code [0,255] : ");
       scanf("%d", &code);
-    } while((code < 0) || (code > 256));
+    } while((code < 0) && (code > 256));
     exit(code);
   }
   else if(pid == -1) {
